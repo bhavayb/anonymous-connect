495ddc889a24a7c2c87c98b83fb932b4500ae8ed
Bhavay Batra <btech10549.23@bitmesra.ac.in>
2025-12-30 17:29:55 +0530

    commit 1

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,32 @@
+import {resend} from '@/lib/resend';
+
+import VerificationEmail from '../../emails/VerificationEmail';
+
+import { ApiResponse } from '@/types/apiResponse';
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+    try{
+
+    await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: email,
+      subject: 'true feedback verification code',
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    
+        return {
+            success: true,
+            message: 'Verification email sent successfully.'
+        }
+    }catch(emailerror){
+        console.log("error sending verification email: ", emailerror);
+        return {
+            success: false,
+            message: 'Failed to send verification email.'
+        }
+    }
+}
